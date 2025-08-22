a40eb7402df69ac06ccfbe56498788968b94580e
Saltyfish <yzykevin@gmail.com>
2025-08-22 10:41:28 +0200

    Update Footer.js

--- themes/landing/components/Footer.js
+++ themes/landing/components/Footer.js
@@ -40,14 +40,6 @@ export default function Footer() {
                 <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
 
                     {/* 1st block */}
-                    {/* <div className="sm:col-span-12 lg:col-span-3">
-                        <div className="mb-2">
-                            <Logo />
-                        </div>
-                        <div className="text-sm text-gray-600">
-                            <SmartLink href="/terms-of-use" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">服务条款</SmartLink> · <SmartLink href="/privacy-policy" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">隐私政策</SmartLink>
-                        </div>
-                    </div> */}
                     <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
                         <h6 className="text-gray-800 font-medium mb-2">Subscribe | 关注</h6>
                         <ul className="text-sm">
